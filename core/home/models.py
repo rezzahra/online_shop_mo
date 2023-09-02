f8539b27c850b5ed9b7060e2bdeb93f21924d839
rezzahra <rezaeizahra@gmail.com>
2023-09-02 09:20:11 +0330

    add app home

---
+++ core/home/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.urls import reverse
+
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    img = models.ImageField(upload_to='products/%Y/%m/%d/')
+    descriptions = models.TextField()
+    price = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home:product_detail', args=[self.slug])
